1f73cb5a578d5ba89a8491643e475e7c21325171
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2026-09-24 18:20:41 +0800

    Integrate `MikroOrmModule` in `PostsModule`, replace `BucketService` with `BucketModule`, and register `Posts` and `Likes` entities

--- api/src/posts/posts.module.ts
+++ api/src/posts/posts.module.ts
@@ -6,13 +6,20 @@
  * version 3 only. See the LICENSE file at the repository root for the full terms.
  */
 import { Module } from '@nestjs/common';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { PostsController } from '@/posts/posts.controller';
 import { PostsService } from '@/posts/posts.service';
 import { AuthModule } from '@/auth/auth.module';
-import { BucketService } from '@/bucket/bucket.service';
+import { BucketModule } from '@/bucket/bucket.module';
+import { Posts } from '@/posts/entities/posts.entity';
+import { Likes } from '@/posts/entities/likes.entity';
 
 @Module({
-  imports: [AuthModule, BucketService],
+  imports: [
+    AuthModule,
+    BucketModule,
+    MikroOrmModule.forFeature([Posts, Likes]),
+  ],
   controllers: [PostsController],
   providers: [PostsService],
 })
